29f006635c1c0f5208bed63be3c7c057d13f7df1
한경만 <kmhan@spectra.co.kr>
2024-08-20 07:03:02 +0900

    update

--- sqlalchemy/sample.py
+++ sqlalchemy/sample.py
@@ -67,3 +67,41 @@ with Session(engine) as session:
     session.add_all([spongebob, sandy, patrick])
 
     session.commit()
+
+from sqlalchemy import select
+
+session = Session(engine)
+
+stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+
+for user in session.scalars(stmt):
+    print(user)
+
+# SELECT with JOIN
+stmt = (
+    select(Address)
+    .join(Address.user)
+    .where(User.name == "sandy")
+    .where(Address.email_address == "sandy@sqlalchemy.org")
+ )
+sandy_address = session.scalars(stmt).one()
+sandy_address
+
+stmt = select(User).where(User.name == "patrick")
+patrick = session.scalars(stmt).one()
+
+patrick.addresses.append(Address(email_address="patrickstar@sqlalchemy.org"))
+
+sandy_address.email_address = "sandy_cheeks@sqlalchemy.org"
+
+session.commit()
+
+sandy = session.get(User, 2)
+
+sandy.addresses.remove(sandy_address)
+
+session.flush()
+
+session.delete(patrick)
+
+session.commit()
